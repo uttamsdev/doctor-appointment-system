3f6c74fb239a79273b9fe6241407fbdcba8f7384
uttamsaha <uttamsaha982@gmail.com>
2022-05-11 20:57:16 +0600

    day picker added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import Navbar from './Pages/Shared/Navbar';
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/appointment' element={<Appointment></Appointment>}></Route>
       </Routes>
     </div>
   );
---
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import chair from "../../assets/images/chair.png";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import { format } from "date-fns";
+
+const AppointmentBanner = () => {
+  const [date, setDate] = useState(new Date()); //geting today's date using new Date()
+  return (
+    //using hero dasiy ui hero
+    <div class="hero min-h-screen">
+      <div class="hero-content flex-col lg:flex-row-reverse">
+        <img src={chair} alt="" class="max-w-sm rounded-lg shadow-2xl" />
+        <div>
+          <DayPicker mode="single" selected={date} onSelect={setDate} />
+          <p>You have selected: {format(date, "PP")}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AppointmentBanner;
