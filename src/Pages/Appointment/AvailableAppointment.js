a651c04c8901513daea1621ae74831dc3f5d21b0
uttamsaha <uttamsaha982@gmail.com>
2022-05-11 21:58:45 +0600

    dynamic modal added

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,22 +1,27 @@
 import { format } from 'date-fns/esm';
 import React, { useEffect, useState } from 'react'
+import BookingModal from './BookingModal';
 import Service from './Service';
 
 const AvailableAppointment = ({date}) => {
     const [services, setServices] = useState([]);
+    const [treatment, setTreatment] = useState(null);
     useEffect(()=>{
         fetch('services.json')
         .then(res => res.json())
         .then(data => setServices(data))
     },[])
+
   return (
     <div>
           <h4 className='text-xl text-secondary text-center'>Available Appointments on: {format(date, "PP")}</h4>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
             {
-                services.map(service => <Service service={service} key={service._id}></Service>)
+                services.map(service => <Service service={service} key={service._id} setTreatment={setTreatment}></Service>)
             }
         </div>
+        {treatment && <BookingModal treatment={treatment}></BookingModal>}
+        {/* jokhon treatement set hobe thokn booking er modal ta open korobo */}
     </div>
   )
 }
--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -1,28 +1,38 @@
-import React from 'react'
+import React from "react";
 
-const Service = ({service}) => {
-    const {name, slots} = service;
+const Service = ({ service, setTreatment }) => {
+  const { name, slots } = service;
 
-    //card from daisyui
+  //card from daisyui
   return (
     <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-  <div class="card-body">
-    <h2 class="card-title text-secondary">{name}</h2>
-    <p>
-        {
-            slots.length > 0 ? <span>{slots[0]}</span>
-            : 
-            <span className='text-red-500'>No slot Available</span>
-        }
-    </p>
-    <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
-    <div class="card-actions justify-center">
-      <button disabled={slots.length===0} class="btn btn-secondary text-white uppercase">Book Appointment</button>
-      {/* slots length 0 hole button disable hobe */}
+      <div class="card-body">
+        <h2 class="card-title text-secondary">{name}</h2>
+        <p>
+          {slots.length > 0 ? (
+            <span>{slots[0]}</span>
+          ) : (
+            <span className="text-red-500">No slot Available</span>
+          )}
+        </p>
+        <p>
+          {slots.length} {slots.length > 1 ? "spaces" : "space"} available
+        </p>
+        <div class="card-actions justify-center">
+          <label
+            disabled={slots.length === 0}
+            class=""
+            onClick={() => setTreatment(service)}
+            for="booking-modal"
+            className="btn btn-secondary text-white uppercase"
+          >
+            Book Appointment
+          </label>
+          {/* slots length 0 hole button disable hobe */}
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-  )
-}
+  );
+};
 
 export default Service;
