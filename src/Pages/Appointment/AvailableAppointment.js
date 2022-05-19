3b61b5d16ed35f79bd6c86ab6d31066189f54b95
uttamsaha <uttamsaha982@gmail.com>
2022-05-19 10:05:48 +0600

    bookings functionality added

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -6,11 +6,12 @@ import Service from './Service';
 const AvailableAppointment = ({date}) => {
     const [services, setServices] = useState([]);
     const [treatment, setTreatment] = useState(null);
+    const formatedDate = format(date, 'PP');
     useEffect(()=>{
-        fetch('http://localhost:5000/service')
+        fetch(`http://localhost:5000/available?date=${formatedDate}`)
         .then(res => res.json())
         .then(data => setServices(data))
-    },[])
+    },[formatedDate])
 
   return (
     <div>
